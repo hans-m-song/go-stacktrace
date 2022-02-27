e8f2425839cf09862adb4ce462c0757f4d502240
Hans Song <hans.song@flightcentre.com>
2022-02-27 21:36:04 +1000

    godoc punctuation and semishallow clone

--- errors.go
+++ errors.go
@@ -10,7 +10,7 @@ var (
 	_ TraceableError    = (*Error)(nil)
 )
 
-// Struct representing an error
+// Struct representing an error.
 type Error struct {
 	Name    string            `json:"name"`
 	Message string            `json:"message"`
@@ -30,11 +30,12 @@ type TraceableError interface {
 	Trace(err error) *Error
 	Tracef(message string, a ...interface{}) *Error
 	Clone() *Error
+	DeepClone() *Error
 }
 
-// Attaches a named value to the metadata of the error
+// Attaches a named value to the metadata of the error.
 //
-// Values get casted into string with `fmt.Sprintf("%+v", value)`
+// Note: values get casted into string with `fmt.Sprintf("%+v", value)`.
 func (e *Error) Add(key string, value interface{}) *Error {
 	e.Meta[key] = fmt.Sprintf("%+v", value)
 	return e
@@ -50,23 +51,30 @@ func (e *Error) GetMeta() []string {
 	return serialiseMetaMap(e.Meta)
 }
 
-// Returns superficial information about the error (name and message)
+// Returns superficial information about the error (name and message).
 //
-// Required to satisfy the error interface
+// Required to satisfy the error interface.
 func (e *Error) Error() string {
 	return serialiseNameMessage(e.Name, e.Message)
 }
 
-// Takes on the message of the given message and traces the callstack
+// Takes on the message of the given message and traces the callstack.
 //
-// Should be called when returning an error
+// Note: should be called when returning an error.
+//
+// Note: will ignore subsequent calls to `Error.Trace` once a stacktrace exists, call `Error.Clone` to create a fresh instance to guarantee a new trace.
 func (e *Error) Trace(err error) *Error {
+	// error has already been traced
+	if len(e.Stack) > 0 {
+		return e
+	}
+
 	e.Stack = trace()
 	e.Message = err.Error()
 	return e
 }
 
-// Convenience function that wraps `fmt.Errorf`, otherwise behaves the same as `Error.Trace`
+// Convenience function that wraps `fmt.Errorf`, otherwise behaves the same as `Error.Trace`.
 //
 // Should be called when returning an error
 func (e *Error) Tracef(message string, a ...interface{}) *Error {
@@ -80,18 +88,25 @@ func (e *Error) String() string {
 	return serialiseError(e)
 }
 
-// Creates a deep-copied clone of the Error
+// Creates a new instance of the Error without a stack trace.
 func (e *Error) Clone() *Error {
 	clone := Error{
 		Name:    e.Name,
 		Message: e.Message,
 		Meta:    make(map[string]string, len(e.Meta)),
-		Stack:   make([]Frame, len(e.Stack)),
 	}
 
-	for key, value := range e.Meta {
-		clone.Meta[key] = value
+	for k, v := range e.Meta {
+		clone.Meta[k] = v
 	}
+
+	return &clone
+}
+
+// Creates a deep-copied clone of the Error including stack trace.
+func (e *Error) DeepClone() *Error {
+	clone := e.Clone()
+	clone.Stack = make([]Frame, len(e.Stack))
 
 	for i, entry := range e.Stack {
 		clone.Stack[i] = Frame{
@@ -101,10 +116,10 @@ func (e *Error) Clone() *Error {
 		}
 	}
 
-	return &clone
+	return clone
 }
 
-// Create a new instance of `Error` with a name
+// Create a new instance of `Error` with a name.
 func New(name string) *Error {
 	return &Error{
 		Name:    name,
